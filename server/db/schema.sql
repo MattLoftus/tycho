40808176d36f6b593c0a3d7a55e7cb3b4d27ae12
Matthew Loftus <Loftus@Matthews-MacBook-Pro-2.local>
2015-11-16 17:42:21 -0800

    Create database schema

---
+++ server/db/schema.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE tycho;
+
+USE tycho;
+
+CREATE TABLE 'users' (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50),
+  `password` varchar(100),
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE 'mission' (
+  'id' int NOT NULL AUTO_INCREMENT,
+  'name' varchar(250),
+  'velocity' float,
+  'heading' float,
+  'velocity' float,
+  'altitude' float,
+  'latitude' float,
+  'longitude' float,
+  'created_at' int,
+  'apogee' float,
+  'perigee' float,
+  'inclination' float,
+  PRIMARY KEY ('id')
+);
+
+CREATE TABLE 'engines' (
+  'id' int NOT NULL AUTO_INCREMENT,
+  'mission_id' int,
+  'stage_num' int,
+  'engine_num' int,
+  'chamber_pressure' float,
+  'force_thrust' float,
+  'created_at' int,
+  'last_updated' int,
+  PRIMARY KEY ('id')
+);
+
+CREATE TABLE 'tanks' (
+  'id' int NOT NULL AUTO_INCREMENT,
+  'mission_id' int,
+  'stage_num' int,
+  'tank_pressure' float,
+  'fuel_volume' float,
+  'fuel_mass' float,
+  'created_at' int, 
+  'last_updated' int,
+  PRIMARY KEY ('id')
+)
+
+CREATE TABLE 'craft' (
+  'id' int NOT NULL AUTO_INCREMENT,
+  'mission_id' int,
+  'power' float,
+  'cabin_pressure' float,
+  'monoprop' float,
+  PRIMARY KEY ('id')
+)
+
+ALTER TABLE `engines` ADD CONSTRAINT `engines_fk0` FOREIGN KEY (`mission_id`) REFERENCES `mission`(`id`);
+
+ALTER TABLE `tanks` ADD CONSTRAINT `tanks_fk0` FOREIGN KEY (`mission_id`) REFERENCES `mission`(`id`);
+
+ALTER TABLE `craft` ADD CONSTRAINT `messages_fk0` FOREIGN KEY (`mission_id`) REFERENCES `mission`(`id`);
+
+
+
